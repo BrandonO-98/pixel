6f25e0b94ed0657d5034305a3616b1b43e27851d
Brandon Oscar Shew <brandonshew98@gmail.com>
2025-01-26 20:53:23 -0500

    Add image properties to product model

--- src/main/java/com/magnify/pixel/controller/ProductController.java
+++ src/main/java/com/magnify/pixel/controller/ProductController.java
@@ -3,15 +3,19 @@ package com.magnify.pixel.controller;
 import com.magnify.pixel.model.Product;
 import com.magnify.pixel.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin
 public class ProductController {
 
-    private ProductService service;
+    private final ProductService service;
 
     @RequestMapping("/")
     public String greet() {
@@ -19,8 +23,24 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<Product> getAllProducts() {
-        return service.getAllProducts()
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+
+        Product product = service.getProduct(id);
+
+        if(product != null)
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+//    @PostMapping("/product")
+//    public ResponseEntity<?> addProduct() {
+//
+//    }
 
 }
--- src/main/java/com/magnify/pixel/service/ProductService.java
+++ src/main/java/com/magnify/pixel/service/ProductService.java
@@ -1,14 +1,24 @@
 package com.magnify.pixel.service;
 
+import com.magnify.pixel.model.Product;
+import com.magnify.pixel.repo.ProductRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
-
+    private final ProductRepo repo;
 
     public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
 
-
+    public Product getProduct(int id) {
+//Since this returns an optional we have to handle when its null
+        return repo.findById(id).orElse(null);
     }
 }
